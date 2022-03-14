933ae7026e5e4f70965c3517c8066255e2d46a91
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-03-14 16:11:07 +0100

    Try different keys

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@
 const main = document.querySelector('main');
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'tolkien';
-const key = '1e19898c87464e239192c8bfe422f280';
-const secret = '4289fec4e962a33118340c888699438d';
+const query = 'burgerschap';
+const key = 'cdb8415c172ec6178b63451e222891a6';
+const secret = '31978bcbb4e5eb7239f01c180e2f07b1';
 const detail = 'Default';
 const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
 
